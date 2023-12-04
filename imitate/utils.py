269e4de8f9b74ee1b2b7ff20741f2fd6d1a24889
Andrea Ghezzi <andrea.ghezzi@imtek.uni-freiburg.de>
2023-12-04 12:13:00 +0100

    add license notice

--- imitate/utils.py
+++ imitate/utils.py
@@ -1,3 +1,25 @@
+"""
+    Learning Nonlinear MPC with Q-loss
+    Imitation Learning from Nonlinear MPC via the Exact Q-Loss and its
+    Gauss-Newton Approximation, IEEE CDC Conference 2023
+    Copyright (C) 2023  Andrea Ghezzi, Jasper Hoffman, Jonathan Frey
+    University of Freiburg, Germany (andrea.ghezzi@imtek.uni-freiburg.de)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
 import logging
 from pathlib import Path
 import random
